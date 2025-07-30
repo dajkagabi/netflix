1fd6e2e47cf8d22d9957822b1b6443ae3ef85dac
dajkagabi <dajkagabi01@gmail.com>
2025-07-30 15:20:28 +0200

    slider

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,23 +8,41 @@ const Home = () => {
       <Hero />
 
       <div className="pt-24 md:pt-32">
-        <MovieRow title="Felkapott most" fetchUrl="/trending/all/week" autoScroll={true} scrollInterval={6000} />
+        <MovieRow
+          title="Felkapott most"
+          fetchUrl="/trending/all/week"
+          autoScroll={true}
+          scrollInterval={6000}
+        />
         <MovieRow
           title="Akció és Kaland"
           fetchUrl="/discover/movie?with_genres=28"
-          autoScroll={true} scrollInterval={6000}
+          autoScroll={true}
+          scrollInterval={6000}
         />
         <MovieRow
           title="Vígjátékok"
           fetchUrl="/discover/movie?with_genres=35"
-          autoScroll={true} scrollInterval={6000}
+          autoScroll={true}
+          scrollInterval={6000}
         />
-        <MovieRow title="Horror" fetchUrl="/discover/movie?with_genres=27" autoScroll={true} scrollInterval={6000} />
-        <MovieRow title="Sci-Fi" fetchUrl="/discover/movie?with_genres=878" autoScroll={true} scrollInterval={6000} />
+        <MovieRow
+          title="Horror"
+          fetchUrl="/discover/movie?with_genres=27"
+          autoScroll={true}
+          scrollInterval={6000}
+        />
+        <MovieRow
+          title="Sci-Fi"
+          fetchUrl="/discover/movie?with_genres=878"
+          autoScroll={true}
+          scrollInterval={6000}
+        />
         <MovieRow
           title="Romantikus"
           fetchUrl="/discover/movie?with_genres=10749"
-          autoScroll={true} scrollInterval={6000}
+          autoScroll={true}
+          scrollInterval={6000}
         />
       </div>
     </div>
